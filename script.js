34bea8dc4626124338057a47a1225bebd6ec0fa9
unknown <silviu_miron_90@outlook.com>
2024-05-22 00:50:17 +0300

    Feature : Create variables and click event.
    -Store classes in variables and create click event
    for submit button.
    -this feature help functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// Store classes in variables
+
+const submitButton = document.querySelector(".submit");
+const buttonNumbers = document.querySelector("button");
+const popUp = document.querySelector(".pop-up");
+
+// Create event for submit button
+submitButton.addEventListener("click", function () {
+  document.querySelector(".submit").style.backgroundColor = "#fff";
+  document.querySelector(".submit").style.color = "hsl(25, 97%, 53%)";
+});
