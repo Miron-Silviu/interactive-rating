fcd427364233f54e481822f64dd7b8197431c6cd
unknown <silviu_miron_90@outlook.com>
2024-05-24 22:58:30 +0300

    Prettier : Modify prettier settings
    -Change from double quotes to single quotes.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 // Store classes in variables
 
-const submitButton = document.querySelector(".submit");
-const buttons = document.querySelectorAll(".button");
-const popUp = document.querySelector(".pop-up");
-const select = document.querySelectorAll(".select");
+const submitButton = document.querySelector('.submit');
+const buttons = document.querySelectorAll('.button');
+const popUp = document.querySelector('.pop-up');
+const select = document.querySelectorAll('.select');
 // Add event for buttons
 
 // const switchNumber = function () {
@@ -15,17 +15,17 @@ const select = document.querySelectorAll(".select");
 // };
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    this.style.backgroundColor = "hsl(216, 12%, 54%)";
-    this.style.color = "#fff";
+  buttons[i].addEventListener('click', function () {
+    this.style.backgroundColor = 'hsl(216, 12%, 54%)';
+    this.style.color = '#fff';
 
     // TODO Add toggle classes
   });
 }
 
 // Create event for submit button
-submitButton.addEventListener("click", function () {
-  document.querySelector(".submit").style.backgroundColor = "#fff";
-  document.querySelector(".submit").style.color = "hsl(25, 97%, 53%)";
-  document.querySelector(".pop-up").classList.remove("hidden");
+submitButton.addEventListener('click', function () {
+  document.querySelector('.submit').style.backgroundColor = '#fff';
+  document.querySelector('.submit').style.color = 'hsl(25, 97%, 53%)';
+  document.querySelector('.pop-up').classList.remove('hidden');
 });
