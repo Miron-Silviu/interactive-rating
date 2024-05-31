317a19dc3311d9ceea9c006356d9f937a11251c5
unknown <silviu_miron_90@outlook.com>
2024-05-31 22:36:38 +0300

    Build : Create switchNumber function

--- script.js
+++ script.js
@@ -9,15 +9,16 @@ const select = document.querySelectorAll('.select');
 // Add event for buttons
 
 // const switchNumber = function () {
-//   this.style.backgroundColor = "hsl(216, 12%, 54%)";
-//   buttons.classList.toggle("select");
-//   select.classList.toggle("buttons");
+//   this.style.backgroundColor = 'hsl(216, 12%, 54%)';
+//   buttons.classList.toggle('select');
+//   select.classList.toggle('buttons');
 // };
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
-    this.style.backgroundColor = 'hsl(216, 12%, 54%)';
-    this.style.color = '#fff';
+    switchNumber();
+    // this.style.backgroundColor = 'hsl(216, 12%, 54%)';
+    // this.style.color = '#fff';
 
     // TODO Add toggle classes
   });
