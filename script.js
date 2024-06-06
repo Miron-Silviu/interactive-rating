92577ba5b3f59c23c9335188f0e68be60621e9ef
unknown <silviu_miron_90@outlook.com>
2024-06-06 23:47:55 +0300

    Delete commented code

--- script.js
+++ script.js
@@ -6,26 +6,11 @@ const submitButton = document.querySelector('.submit');
 const buttons = document.querySelectorAll('input');
 const popUp = document.querySelector('.pop-up');
 const select = document.querySelectorAll('label');
+
 // Add event for buttons
 
-// const init = function () {
-//   select.classList.add('button');
-//   buttons.classList.remove('input');
-// };
-
-// const switchNumber = function () {
-//   select.classList.toggle('input');
-//   buttons.classList.toggle('button');
-// };
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-    // this.classList.toggle('input');
-    // this.classList.toggle('label');
-    // this.style.color = '#fff';
-    // this.style.backgroundColor = 'hsl(216, 12%, 54%)';
-    // TODO Add toggle classes
-    // switchNumber();
-  });
+  buttons[i].addEventListener('click', function () {});
 }
 
 // Create event for submit button
