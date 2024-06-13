d309a364d4737ef529e8e708326d0e670a390dcc
unknown <silviu_miron_90@outlook.com>
2024-06-13 20:01:20 +0300

    Modify index and add rename info constant.

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const submitButton = document.querySelector('.submit');
 const popUp = document.querySelector('.pop-up');
 const select = document.querySelectorAll('label');
 const radioButton = document.querySelectorAll('.input-radio');
-const info = (document.querySelector('.info').textContent = 'You selected');
+const info = (document.querySelector('.information').textContent =
+  'You selected');
 
 // Add event for buttons
 
 for (let i = 0; i < radioButton.length; i++) {
   radioButton[i].addEventListener('click', function () {});
 }
-
 // Create event for submit button
 submitButton.addEventListener('click', function () {
   document.querySelector('.submit').style.backgroundColor = '#fff';
